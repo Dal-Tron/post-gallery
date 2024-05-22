a41dfdf2513602800044f5a2e33add483ced6d87
inboxDallas <inboxDallas@gmail.com>
2024-05-22 11:47:42 -0700

    feat: add a home button to the layout

--- src/components/feature/Layout/index.tsx
+++ src/components/feature/Layout/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { Link, Outlet } from 'react-router-dom'
 
 const Layout: React.FC = () => {
   return (
     <div className="flex h-full flex-col">
       <header className="bg-gray-800 p-4 text-white">
-        <h1>Post Gallery</h1>
+        <Link to="/">
+          <h1>Post Gallery</h1>
+        </Link>
       </header>
-      <main className="grow">
+      <main className="mb-8 grow">
         <Outlet />
       </main>
       <footer className="bg-gray-800 p-4 text-white">
