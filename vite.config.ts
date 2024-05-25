64d63dbb597e2ed0ad01a827d4f5f94248d599bc
inboxDallas <inboxDallas@gmail.com>
2024-05-25 16:27:58 -0700

    build: breaking up build chunks to imporve initial loading time

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,29 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
+import { dependencies } from './package.json'
+function renderChunks(deps: Record<string, string>) {
+  const chunks = {}
+  Object.keys(deps).forEach((key) => {
+    if (['react', 'react-router-dom', 'react-dom'].includes(key)) return
+    chunks[key] = [key]
+  })
+  return chunks
+}
+
 // https://vitejs.dev/config https://vitest.dev/config
 export default defineConfig({
+  build: {
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-router-dom', 'react-dom'],
+          ...renderChunks(dependencies)
+        }
+      }
+    }
+  },
   plugins: [react(), tsconfigPaths()],
   test: {
     globals: true,
